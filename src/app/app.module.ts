7789e5a9357e14aea3912253382a5ee9c5b37c93
sajan.sainju <sajan.sainju@teamcoda.com>
2021-04-11 00:49:21 +0545

    added angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { SharedModule } from './shared/shared.module';
     FormsModule,
     SharedModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
   ],
   bootstrap: [AppComponent]
 })
